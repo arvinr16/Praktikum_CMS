a0d9be06cc4c390244b3450925b5f2cef0c3b46e
arvinr16 <arviansyahyk@gmail.com>
2026-04-26 02:15:45 +0700

    Add for testing 'test-db'

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "Koneksi Berhasil!";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
+});
