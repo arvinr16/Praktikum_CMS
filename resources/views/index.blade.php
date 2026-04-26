4141aa6966e3261bad8e9dcb3c15f43bbda7e24b
arvinr16 <arviansyahyk@gmail.com>
2026-04-26 13:52:47 +0700

    Adding 'index.blade.php' in 'resources/views'

---
+++ resources/views/index.blade.php
@@ -0,0 +1,500 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Prodi Sistem Informasi - Home</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="images/logoWebSI_gradient.png" />
+
+    <!-- Link untuk Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Link untuk CSS Boostrap CDN -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+      crossorigin="anonymous"
+    />
+
+    <!-- Link untuk CSS Lokal -->
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <!-- Navbar Section Start -->
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-transparent shadow sticky-top navbar-blur"
+    >
+      <div class="container">
+        <img
+          src="images/logoWebSI.webp"
+          class="nav-img-logo"
+          alt="Logo Web Prodi"
+        />
+        <a
+          href="#"
+          class="fw-bold text-decoration-none text-nav"
+          style="
+            background: linear-gradient(16deg, #6199e2, #933dcd);
+            background-clip: text;
+            -webkit-text-fill-color: transparent;
+          "
+        >
+          Sistem Informasi</a
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+            <li class="nav-item">
+              <a class="nav-link" href="kurikulum.html">Kurikulum</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dosen.html">Dosen</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dokumen.html">Dokumen</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kontak.html">Kontak</a>
+            </li>
+          </ul>
+          <i id="themeIcon" class="theme-icon">🌙</i>
+          <div class="form-check form-switch d-flex align-items-center gap-2">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="themeToggle"
+            />
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar Section End -->
+
+    <!-- Landing Section Start -->
+    <section id="landing">
+      <div class="container">
+        <div class="row g-4 justify-content-center">
+          <div class="col-md-6">
+            <h1 class="h1-landing">
+              Selamat Datang di Website Resmi Prodi Sistem Informasi UMBY
+            </h1>
+            <p class="text-muted lead">
+              Kami percaya bahwasanya untuk membangun Sumber Daya Manusia yang
+              unggul, diperlukan metode pembelajaran yang mumpuni, seperti yang
+              sudah kita lakukan di Prodi Sistem Informasi ini.
+            </p>
+            <a href="kontak.html#pendaftaran" class="btn-landing"
+              >Daftar Sekarang</a
+            >
+          </div>
+          <img
+            src="https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29sbGVnZXxlbnwwfHwwfHx8MA%3D%3D"
+            class="img-landing shadow"
+            alt="Gambar Diskusi Mahasiswa"
+          />
+        </div>
+      </div>
+      <a href="#about" class="navigation"
+        ><img class="bounce" src="images/arrowDown_blue_2.png" alt="arrowDown"
+      /></a>
+    </section>
+    <!-- Landing Section End -->
+
+    <!-- About Section Start -->
+    <section id="about">
+      <div class="container">
+        <div class="row g-4 justify-content-center" style="cursor: default">
+          <!-- Kartu akreditasi -->
+          <div class="col-lg-4">
+            <div
+              class="card about-card border-3 border-primary-subtle shadow text-center p-4 align-items-center h-100"
+            >
+              <img
+                src="images/letter_A_icon.png"
+                class="img-about"
+                alt="Letter A"
+              />
+              <h5 class="card-title fw-bold">Akreditasi A</h5>
+              <p class="card-text">
+                Program studi ini telah memperoleh Akreditasi A sebagai bentuk
+                pengakuan atas mutu penyelenggaraan pendidikan, serta komitmen
+                berkelanjutan dalam menjaga dan meningkatkan kualitas akademik.
+              </p>
+            </div>
+          </div>
+          <!-- Kartu lulusan -->
+          <div class="col-lg-4">
+            <div
+              class="card about-card border-3 border-primary-subtle shadow text-center p-4 align-items-center h-100"
+            >
+              <img
+                src="images/graduationCapIcon.png"
+                class="img-about"
+                alt="Graduation Cap Icon"
+              />
+              <h5 class="card-title fw-bold">Durasi Studi Cepat</h5>
+              <p class="card-text">
+                Mahasiswa memiliki kesempatan untuk menyelesaikan studi dalam
+                waktu relatif singkat, yaitu sekitar tiga tahun, dengan memenuhi
+                persyaratan akademik yang telah ditetapkan.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div
+              class="card about-card border-3 border-primary-subtle shadow text-center p-4 align-items-center h-100"
+            >
+              <img
+                src="images/graduationCertificateIcon.png"
+                class="img-about"
+                alt="Graduation Certificate Icon"
+              />
+              <h5 class="card-title fw-bold">Gelar Lulusan</h5>
+              <p class="card-text">
+                Lulusan program studi ini akan memperoleh gelar akademik Sarjana
+                Komputer (S.Kom) sebagai bukti kompetensi dan capaian akademik
+                di bidang teknologi informasi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="#vision" class="navigation"
+        ><img class="bounce" src="images/arrowDown_blue_2.png" alt="arrowDown"
+      /></a>
+    </section>
+    <!-- About Section End -->
+
+    <!-- Vision Section Start -->
+    <section id="vision">
+      <div class="container">
+        <div class="row gap-4 justify-content-center" style="cursor: default">
+          <!-- Kartu visi -->
+          <div class="col-md-5">
+            <div
+              class="card border-0 shadow text-center p-4 align-items-center h-100"
+            >
+              <img
+                src="images/visionMissionIcon_1.png"
+                class="img-about"
+                alt="Letter A"
+              />
+              <h5 class="card-title fw-bold">Visi</h5>
+              <p class="card-text">
+                Menjadi Program Studi yang unggul dalam bidang sistem informasi
+                berbasis Artificial Intelligence di tahun 2029.
+              </p>
+            </div>
+          </div>
+          <!-- Kartu misi -->
+          <div class="col-md-5">
+            <div
+              class="card border-0 shadow text-center p-4 align-items-center h-100"
+            >
+              <img
+                src="images/visionMissionIcon_2.png"
+                class="img-about"
+                alt="Letter A"
+              />
+              <h5 class="card-title fw-bold">Misi</h5>
+              <p class="card-text">
+                1. Menyelenggarakan pendidikan yang berkualitas di bidang sistem
+                informasi berbasis Artificial Intelligence. <br />2.
+                Menghasilkan lulusan di bidang sistem informasi yang profesional
+                dan berkontribusi pada kemajuan ilmu pengetahuan dan teknologi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="#course" class="navigation"
+        ><img class="bounce" src="images/arrowDown_blue_2.png" alt="arrowDown"
+      /></a>
+    </section>
+    <!-- Vision Section End -->
+
+    <!-- Course Section Start -->
+    <section id="course">
+      <div class="container">
+        <div class="slider-container">
+          <div class="slider" id="slider">
+            <div class="card-slider">Web Design Interface</div>
+            <div class="card-slider">Database</div>
+            <div class="card-slider">Algoritma dan Pemrograman</div>
+            <div class="card-slider">Riset Operasi</div>
+            <div class="card-slider">Sistem Informasi Manajemen</div>
+            <div class="card-slider">UI/UX</div>
+          </div>
+        </div>
+      </div>
+      <a href="#news" class="navigation"
+        ><img class="bounce" src="images/arrowDown_blue_2.png" alt="arrowDown"
+      /></a>
+    </section>
+    <!-- Course Section End -->
+
+    <!-- News Section Start -->
+    <section id="news">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <div class="card card-news shadow h-100">
+              <div class="card-header" style="border-radius: 20px 20px 0 0">
+                <form class="d-flex mb-1 form-news">
+                  <input
+                    type="search"
+                    class="form-control form-control-sm me-2"
+                    placeholder="Cari Berita..."
+                    aria-label="Search blog"
+                  />
+                  <a href="#" type="submit" class="button-search">
+                    <img
+                      src="images/searchIconBlue.png"
+                      alt="Search Icon"
+                      class="search"
+                    />
+                  </a>
+                </form>
+                <h2 class="title-section">Pengumuman</h2>
+              </div>
+              <div class="card-body">
+                <ol>
+                  <li>
+                    <a
+                      href="jadwal_uas.pdf"
+                      class="text-reset text-decoration-none"
+                      download="jadwal_uas.pdf"
+                    >
+                      Download Jadwal UAS 2026
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="jadwal_uts.pdf"
+                      class="text-reset text-decoration-none"
+                      download="jadwal_uts.pdf"
+                    >
+                      Download Jadwal UTS 2025
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="pengumumanLombaDiesNatalies.pdf"
+                      class="text-reset text-decoration-none"
+                      download="pengumumanLombaDiesNatalies.pdf"
+                    >
+                      Download Jadwal Lomba Dies Natalies 2025
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="jadwalPendaftaranMaBa.pdf"
+                      class="text-reset text-decoration-none"
+                      download="jadwalPendaftaranMaBa.pdf"
+                    >
+                      Jadwal Pendaftaran Mahasiswa Baru
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="text-reset text-decoration-none">
+                      Pengumuman lain-lain...
+                    </a>
+                  </li>
+                </ol>
+              </div>
+              <div class="card-header">
+                <h2 class="title-section">Berita Akademik</h2>
+              </div>
+              <div class="card-body">
+                <ul>
+                  <li>
+                    <a href="#" class="text-reset text-decoration-none"
+                      >Seminar IT Nasional</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="text-reset text-decoration-none"
+                      >Seminar SI Nasional</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="text-reset text-decoration-none"
+                      >Prestasi Mahasiswa</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="text-reset text-decoration-none"
+                      >Lomba Nasional</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="text-reset text-decoration-none"
+                      >Berita lainnya...</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="#navigation-page" class="navigation"
+        ><img class="bounce" src="images/arrowDown_blue_2.png" alt="arrowDown"
+      /></a>
+    </section>
+    <!-- News Section End -->
+
+    <!-- Navigation Page Section Start -->
+    <section id="navigation-page" class="mb-5">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="card card-nav shadow-sm">
+            <div class="card-header" style="border-radius: 20px 20px 0 0">
+              <h1 class="title-section fw-bold text-center">Navigation Page</h1>
+            </div>
+            <div class="card-body card-nav-body">
+              <div
+                class="d-flex flex-column flex-md-row justify-content-center align-items-center gap-3"
+              >
+                <div>
+                  <a href="kurikulum.html" class="nav-page">Page Kurikulum</a>
+                </div>
+                <h3 class="text-muted">or</h3>
+                <div>
+                  <a href="dosen.html" class="nav-page">Page Dosen</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Navigation Page Section End -->
+
+    <!-- Footer Section Start -->
+    <footer class="bg-dark-subtle py-5">
+      <div class="container">
+        <div
+          class="d-flex flex-column flex-md-row justify-content-center gap-3"
+        >
+          <!-- Social Icon -->
+          <div class="col-lg-3">
+            <img
+              src="images/logoWebSI.webp"
+              class="footer-img"
+              alt="Logo Web Prodi"
+            />
+            <p>
+              Program Studi Sistem Informasi, Fakultas Teknologi Informasi,
+              Universitas Mercu Buana Yogyakarta
+            </p>
+            <div class="social-icon-footer">
+              <ul class="ps-0 mt-2 mb-2">
+                <a href="#"
+                  ><img
+                    src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"
+                    alt="Icon Facebook"
+                /></a>
+                <a href="#"
+                  ><img
+                    src="https://cdn-icons-png.flaticon.com/128/5969/5969020.png"
+                    alt="Icon X/ Twitter"
+                /></a>
+                <a href="#"
+                  ><img
+                    src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"
+                    alt="Icon Instagram"
+                /></a>
+              </ul>
+            </div>
+          </div>
+          <!-- Contact Info -->
+          <div class="col-lg-3">
+            <h5 class="fw-bold">CONTACT INFO</h5>
+            <div class="address">
+              <p class="mb-1"><strong>ADDRESS:</strong></p>
+              <ul class="list-wrap">
+                <li>
+                  <div class="content">
+                    <a
+                      class="link-body-emphasis"
+                      href="https://maps.app.goo.gl/dyjLnyFB5PdnVKN86"
+                      >Kampus I: Jl. Wates Km. 10 Yogyakarta 55753. Telp:
+                      0274-6498212, 6498211, Fax. 0274-6498213.
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div class="content">
+                    <a
+                      class="link-body-emphasis"
+                      href="https://maps.app.goo.gl/iHaXKAn4QYr73jpF8"
+                      >Kampus II: Jl. Jembatan Merah, No. 84.C. Gejayan,
+                      Yogyakarta. Telp: 0274-584922
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div class="content">
+                    <a
+                      class="link-body-emphasis"
+                      href="https://maps.app.goo.gl/3oecTLyFHW8qvopXA"
+                      >Kampus III: Jl. Ring Road Utara, Depok, Sleman,
+                      Yogyakarta, Telp: 0274-2801918, 2801900.
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <p class="mb-1"><strong>PHONE:</strong> (0274)-6498212</p>
+            <p class="mb-1">
+              <strong>EMAIL:</strong> mercubuana-yogya@gmail.com
+            </p>
+            <p class="mb-1">
+              <strong>WORKING DAYS/HOURS:</strong> Mon - Fri / 8:00 AM - 3:30 PM
+            </p>
+          </div>
+        </div>
+        <hr style="background: black; height: 1px; border-radius: 50px" />
+        <div class="d-flex justify-content-between align-items-center">
+          <p class="mb-0">
+            &copy; Prodi Sistem Informasi UMBY (ArviNR) 2025. All Rights
+            Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Link untuk JS Bootstrap CDN -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+      crossorigin="anonymous"
+    ></script>
+    <!-- Link untuk JS Lokal -->
+    <script src="main.js"></script>
+  </body>
+</html>
