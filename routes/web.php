8d6e33e5c5d24ead81f06cb4b63d403279ac21af
arvinr16 <arviansyahyk@gmail.com>
2026-04-30 00:35:46 +0700

    Installing 'breeze', 'blade' in 'Authentication' materials

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,16 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
-use Illuminate\Support\Facades\DB;
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/test-db', function () {
-    try {
-        DB::connection()->getPdo();
-        return "Koneksi Berhasil!";
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
