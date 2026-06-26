e88c80fec7f8f90eee24c99d10ce6dbf12fe1cbf
arvinr16 <arviansyahyk@gmail.com>
2026-06-27 01:58:52 +0700

    Add editing 'app/Http/Controllers/Front/ArticleController.php' for restructuring

---
+++ app/Http/Controllers/Front/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $article = Article::with('category')->latest()->paginate(9);
+        return view('articles.index', compact('articles'));
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', compact('article'));
+    }
+}
