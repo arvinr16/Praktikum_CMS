72cdcff3b286caeaf70692131b6f8b7332687c85
arvinr16 <arviansyahyk@gmail.com>
2026-06-30 07:30:00 +0700

    Add model revision in 'app/Models/Message.php'

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    protected $fillable = ['name', 'email', 'message'];
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'message',
+        'is_read'
+    ];
+
+    // Cast boolean: $message->is_read => true/false (bukan 0/1)
+    protected $casts = [
+        'is_read'   => 'boolean'
+    ];
+
+    // Scope: Message::unread()->get()
+    public function scopeUnread($query)
+    {
+        return $query->where('is_read', false);
+    }
+
+    // Scope: Message::read()->get()
+    public function scopeRead($query)
+    {
+        return $query->where('is_read', true);
+    }
 }
